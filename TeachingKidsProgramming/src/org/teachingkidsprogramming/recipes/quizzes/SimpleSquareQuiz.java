43ff16dc9a73a3c41861774c0b78f221b43929e8
Erin Moon <kongjui.m@hotmail.com>
2013-06-18 15:51:01 -0700

    We Are Amazingly Smart (wooo, A+!) andsoisPudgy-Erin/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/SimpleSquareQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/SimpleSquareQuiz.java
@@ -1,5 +1,7 @@
 package org.teachingkidsprogramming.recipes.quizzes;
 
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SimpleSquareQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.SquareQuiz;
 
@@ -7,19 +9,19 @@ public class SimpleSquareQuiz implements SquareQuiz
 {
   public void question1()
   {
-    //  Turn the tortoise 1/5 of 360 degrees to the right
+    Tortoise.turn(360 / 5);
   }
   public void question2()
   {
-    //  Move the tortoise 110 pixels
+    Tortoise.move(110);
   }
   public void question3()
   {
-    //  Change the color the tortoise draws to yellow
+    Tortoise.setPenColor(Colors.Yellows.Yellow);
   }
   public void question4()
   {
-    //  Change the width of the line the tortoise draws to 5 pixels
+    Tortoise.setPenWidth(5);
   }
   public static void main(String[] args)
   {
