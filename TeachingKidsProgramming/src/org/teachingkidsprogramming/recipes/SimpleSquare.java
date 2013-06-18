ae1cf33dc5d1846305f55cf9cfefb76a943761ec
Erin Moon <kongjui.m@hotmail.com>
2013-06-18 14:07:31 -0700

    Our Baby Turtle-Erin/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -1,5 +1,6 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
 
 public class SimpleSquare
@@ -9,9 +10,17 @@ public class SimpleSquare
     Tortoise.show();
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
-    //      Change the color of the line the tortoise draws to "blue" --#4
+    Tortoise.setPenColor(Colors.Blues.CadetBlue);
     Tortoise.move(50);
-    //      Turn the tortoise to the right (90 degrees) --#3
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.CadetBlue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.CadetBlue);
+    Tortoise.move(50);
+    Tortoise.turn(90);
+    Tortoise.setPenColor(Colors.Blues.CadetBlue);
+    Tortoise.move(50);
     Tortoise.turn(90);
     //  Repeat --#5.2
   }
