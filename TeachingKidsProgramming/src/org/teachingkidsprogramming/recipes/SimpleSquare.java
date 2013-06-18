f847f191dea348ac7dcf2d012e76a6dbbbc9349e
Erin Moon <kongjui.m@hotmail.com>
2013-06-18 14:14:08 -0700

    Our Turtle Pudgy is Created-Erin/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -8,20 +8,12 @@ public class SimpleSquare
   public static void main(String[] args) throws Exception
   {
     Tortoise.show();
-    //  Make the tortoise move as fast as possible --#6
-    //  Do the following 4 times --#5.1
-    Tortoise.setPenColor(Colors.Blues.CadetBlue);
-    Tortoise.move(50);
-    Tortoise.turn(90);
-    Tortoise.setPenColor(Colors.Blues.CadetBlue);
-    Tortoise.move(50);
-    Tortoise.turn(90);
-    Tortoise.setPenColor(Colors.Blues.CadetBlue);
-    Tortoise.move(50);
-    Tortoise.turn(90);
-    Tortoise.setPenColor(Colors.Blues.CadetBlue);
-    Tortoise.move(50);
-    Tortoise.turn(90);
-    //  Repeat --#5.2
+    Tortoise.setSpeed(10);
+    for (int i = 1; i <= 4; i++)
+    {
+      Tortoise.setPenColor(Colors.Blues.Blue);
+      Tortoise.move(50);
+      Tortoise.turn(90);
+    }
   }
 }
