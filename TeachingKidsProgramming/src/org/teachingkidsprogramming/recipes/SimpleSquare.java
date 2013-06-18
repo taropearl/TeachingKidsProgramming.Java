0e937002d90c0752448259590fa059ce5187cf77
Erin Moon <kongjui.m@hotmail.com>
2013-06-18 15:23:06 -0700

    Pudgy is CURIOUS about the number of Chantalle's boyfriends -Erin/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -2,6 +2,7 @@ package org.teachingkidsprogramming.recipes;
 
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.windows.MessageBox;
 
 public class SimpleSquare
 {
@@ -9,13 +10,13 @@ public class SimpleSquare
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    int sides = 10;
+    int sides = MessageBox.askForNumericalInput("How many boyfriends do you have??");
     for (int i = 1; i <= sides; i++)
     {
       Tortoise.setPenColor(Colors.getRandomColor());
-      Tortoise.setPenWidth(9);
-      Tortoise.move(75);
-      Tortoise.turn(360.0 / sides);
+      Tortoise.setPenWidth(i * 2);
+      Tortoise.move(i * -5);
+      Tortoise.turn(360.0 * 3 / sides);
     }
   }
 }
