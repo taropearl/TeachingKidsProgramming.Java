e1f1e4460c39203626af2e16deb4626c3c76efaa
Maverick Chen <mavelche@gmail.com>
2013-06-25 14:01:40 -0700

    Completed spiral - Maverick/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Spiral.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/Spiral.java
@@ -1,18 +1,31 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors.Purples;
+import org.teachingextensions.logo.Tortoise;
 
 public class Spiral
 {
   public static void main(String[] args)
   {
-    //    Show the tortoise --#1
-    //    Make the tortoise go as fast as possible --#4
-    //    Add Blue Violet to the Color Wheel --#6
-    //    Add Violet to the Color Wheel --#8
-    //    Add Purple to the Color Wheel --#9
-    //    Do the following 75 times --#3
-    //         Change the color of the line the tortoise draws the next color on the Color Wheel --#7
-    //         Move the tortoise 5 times the current line number you are drawing --#5
-    //         Turn the tortoise 1/3 of 360 degrees to the right --#2
+    //colorwheel
+    ColorWheel.addColor(Purples.BlueViolet);
+    ColorWheel.addColor(Purples.Violet);
+    ColorWheel.addColor(Purples.Purple);
+    //default code
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    int linenumber = 0;
+    //drawing
+    for (int i = 1; i <= 75; i++)
+    {
+      Tortoise.setPenColor(ColorWheel.getNextColor());
+      //         Move the tortoise 5 times the current line number you are drawing --#5
+      Tortoise.move(5 * linenumber++);
+      Tortoise.turn(360 / 3);
+    }
+  }
+  private static void linenumber()
+  {
   }
 }
