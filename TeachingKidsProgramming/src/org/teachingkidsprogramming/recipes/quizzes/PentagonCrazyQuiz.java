1156688edd915b6b280ff24889f998b13703af00
Maverick Chen <mavelche@gmail.com>
2013-06-25 15:58:24 -0700

    PentagonCrazyQuiz -- Maverick/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/PentagonCrazyQuiz.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/quizzes/PentagonCrazyQuiz.java
@@ -1,26 +1,31 @@
 package org.teachingkidsprogramming.recipes.quizzes;
 
+import org.teachingextensions.logo.ColorWheel;
+import org.teachingextensions.logo.Colors;
+import org.teachingextensions.logo.Tortoise;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonCrazyQuizGrader;
 import org.teachingkidsprogramming.recipes.quizzes.graders.PentagonQuiz;
 
 public class PentagonCrazyQuiz extends PentagonCrazyQuizGrader
 {
-  //      Question1
-  //      Create a method called thread
-  //       that moves the tortoise 6 pixels
+  public void thread()
+  {
+  }
   public void question2()
   {
-    //        Do the following 76 times
-    //        Quiz.Stitch()
-    //        Repeat
+    for (int i = 1; i <= 76; i++)
+    {
+      Tortoise.move(6);
+      stitch();
+    }
   }
   public void question3()
   {
-    //        Add lime to the color wheel
+    ColorWheel.addColor(Colors.Greens.Lime);
   }
   public void question4()
   {
-    //        Add red to the color wheel
+    ColorWheel.addColor(Colors.Reds.Red);
   }
   public static void main(String[] args)
   {
