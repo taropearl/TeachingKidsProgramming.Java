19951d2eb66864d71a2443748bf6a60188c08dfa
Chantalle B <chantalle_briL@yahoo.com>
2013-06-26 14:41:21 -0700

    Completed adlibs program - Mauricio/Chantalle

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/AdLibs.java
@@ -1,17 +1,19 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.simpleparser.Parser;
+import org.teachingextensions.utils.Viewer;
+import org.teachingextensions.windows.MessageBox;
 
 public class AdLibs
 {
   public static void main(String[] args)
   {
-    //Ask the user to enter an adverb, save it as currentAdverb --#2
-    //Ask the user to enter a verb ending in '-ed', save it as currentEdVerb --#4
-    //Ask the user to enter a body part, save it as currentBodyPart --#6
-    //Set the value of the currentStory to the word "Today " --#1.2
-    //Add the words "I woke " + currentAdverb + ". " to the currentStory --#3
-    //Add the words '"Then I " + currentEdVerb + " " to the currentStory --#5
-    //Add the words "my " + currentBodyPart + ". " to the current story --#7
-    //Show the currentStory in a message box as a message --#1.1
+    words words = new words();
+    words.adverb = MessageBox.askForTextInput("Please enter an adverb");
+    words.verbed = MessageBox.askForTextInput("Please enter a verb ending in -ed");
+    words.bodyPart = MessageBox.askForTextInput("Please enter a body part");
+    String story = Parser.parseRtfFile("view.rtf", words);
+    //MessageBox.showMessage(story);
+    Viewer.displayRtfFile(story);
   }
 }
---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/words.java
@@ -0,0 +1,8 @@
+package org.teachingkidsprogramming.recipes;
+
+public class words
+{
+  public String adverb;
+  public String verbed;
+  public String bodyPart;
+}
